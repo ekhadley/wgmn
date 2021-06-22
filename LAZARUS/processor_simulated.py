2e8e5ad126a8b423f204bcd05692547908906228
Ethan_Hadley <ekhadley@gmail.com>
2021-06-22 16:50:36 -0500

    fgbfg

--- LAZARUS/processor_simulated.py
+++ LAZARUS/processor_simulated.py
@@ -42,22 +42,22 @@ class reader():
     def getTile(self, src):
         mask = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(mask, self.lowerYel, self.upperYel)
-        mask = cv2.erode(mask, np.ones((4, 4), np.uint8))
-        mask = cv2.dilate(mask, np.ones((4, 4), np.uint8))
+       # mask = cv2.erode(mask, np.ones((4, 4), np.uint8))
+       # mask = cv2.dilate(mask, np.ones((4, 4), np.uint8))
         return mask[self.y1:self.y2, self.x1:self.x2]
     def getMask(self, src):
         mask = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(mask, self.lowerYel, self.upperYel)
-        mask = cv2.erode(mask, np.ones((4, 4), np.uint8))
-        mask = cv2.dilate(mask, np.ones((4, 4), np.uint8))
+      #  mask = cv2.erode(mask, np.ones((4, 4), np.uint8))
+      #  mask = cv2.dilate(mask, np.ones((4, 4), np.uint8))
         return mask
     def getCenter(self, src):
         centerStart = 0
         centerEnd = 0
         mask = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
         mask = cv2.inRange(mask, self.lowerYel, self.upperYel)
-        mask = cv2.erode(mask, np.ones((4, 4), np.uint8))
-        mask = cv2.dilate(mask, np.ones((4, 4), np.uint8))
+     #   mask = cv2.erode(mask, np.ones((4, 4), np.uint8))
+     #   mask = cv2.dilate(mask, np.ones((4, 4), np.uint8))
         cut = mask[self.y1:self.y2, self.x1:self.x2]        
         for i in range(1, (self.x2 - self.x1) - 1):
             if cut[21][i] == 0 and cut[21][i+1] == 255:
@@ -79,25 +79,15 @@ read = reader()
 x = 0
 tmp = 0
 itg = 0
-count = 5000
-delay = 0
+count = 3000
+delay = 1
 
 while 1:
-#   frame counting    
-    if 1:
-        if keyboard.is_pressed('up'):
-            delay += .003
-        if keyboard.is_pressed('down'):
-            delay -= .003
-        if delay < 0:
-            delay = 0    
-        time.sleep(delay)
+    count += 1
+    if count == 8595:
+        count = 3000
 
-        count += 3
-        if count >= 13400:
-            count = 5000
-
-    frame = np.array(cv2.imread('C:\\Users\\ekhad\\Desktop\\carshit\\td1\\frame' + str(count) + '.png'))
+    frame = np.array(cv2.imread('D:\\lvid\\frame' + str(count) + ".png"))
 
     cut = read.getTile(frame)
     mask = read.getMask(frame)
@@ -113,12 +103,12 @@ while 1:
         x = int(x/10)
 
         avg = 0
-        if len(prevs) < 1000:
-            prevs.append(x)
-            prevs.pop(0)
-            for i in prevs:
-                avg += i
-            avg /= (len(prevs) + .0001)
+
+        prevs.append(x)
+        prevs.pop(0)
+        for i in prevs:
+            avg += i
+        avg /= (len(prevs) + .0001)
 
         diff = avg - x
 
@@ -133,7 +123,10 @@ while 1:
 
         vel = Vf - Vi
 
-        velDiffScaled = (vel**1.2)/diff
+        try:
+            velDiffScaled = round((vel**2)/diff)
+        except Exception:
+            velDiffScaled = 0
 
         if diff < 0:
             itg -= .01*diff
@@ -144,7 +137,7 @@ while 1:
         itg = limit(itg, -20, 20)
         vals = [diff, itg, velDiffScaled]
 
-    ctrls.append(PID(vals, 100, 180, 300))
+    ctrls.append(PID(vals, 50, 180, 2000))
     ctrls.pop(0)
     ctrl = 0
     for i in ctrls:
@@ -152,15 +145,21 @@ while 1:
     ctrl = int(ctrl/10)
     ctrl = limit(ctrl, -50, 50)
 
-    qaz.write(str(ctrl).encode("utf-8"))
+    try:
+        qaz.write(str(ctrl).encode("utf-8"))
+    except Exception:
+        print("transfer failed")
 
 #   adding dots
     cv2.line(frame, (265, 280), (265 + ctrl, 260), (200, 200, 20), 2)
+    cv2.line(frame, (265, 350), (265 + round(20*vel/diff), 350), (200, 50, 120), 4)
     cv2.circle(frame, (x, 280), 2, (200, 20, 20), 4)
+    cv2.circle(cut, (x, 5), 2, (200, 20, 20), 4)
     cv2.circle(frame, (int(avg), 280), 7, (20, 200, 20), 2)
     cv2.putText(frame, str(count), (15, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (180, 30, 180), 2, cv2.LINE_AA)
 
     cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
     cv2.imshow('cut', cut)
 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
