2ab9599304b0044d2f1476628cf3c10a9267b980
Ethan_Hadley <ekhadley@gmail.com>
2021-06-22 14:33:05 -0500

    cdfd

--- LAZARUS/playback.py
+++ LAZARUS/playback.py
@@ -1,19 +1,22 @@
 import numpy, time, cv2, time
 from PIL import Image
 
-count = 0
+count = 9
 
 
 while count > -1:
     time.sleep(0)
     count += 1
 
-    path = 'D:\\LAZARUS\\frame' + str(count) + ".png"
+    path = 'D:\\lvid\\frame' + str(count) + ".png"
 
-    ret, frame = cv2.imread(path)
+    frame = cv2.imread(path)
 
     print(path)
-    cv2.imshow('frame', frame)
+    try:
+        cv2.imshow('frame', frame)
+    except Exception:
+        pass
 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         1
