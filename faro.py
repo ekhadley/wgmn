4c03e9e6e67b531b5626413d4fbbcfd7419f7476
Ethan_Hadley <ekhadley@gmail.com>
2021-08-07 10:15:41 -0500

    ghjgfhj

--- faro.py
+++ faro.py
@@ -7,17 +7,15 @@ while 1:
     stime = time.time()
     ret, frame = vid.read()
     cv2.cvtColor(frame, cv2.COLOR_RGB2BGRA)
-    '''
+
     faces = fr.face_locations(frame)
 
-    print(faces)
-
     if len(faces) > 0:
-        cv2.rectangle(frame, (faces[0][0], faces[0][1]), (faces[0][2], faces[0][3]), (250, 10, 50), 3)
-    '''
+        cv2.rectangle(frame, (faces[0][1], faces[0][0]), (faces[0][3], faces[0][2]), (250, 10, 50), 3)
+
     cv2.imshow('frame', frame)
 
-    #print(1/(time.time()-stime))
+    print(1/(time.time()-stime))
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         1
 
