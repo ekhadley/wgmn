c7deb91de45312a73de80ea2a8e1051db2088807
Ethan_Hadley <ekhadley@gmail.com>
2021-08-07 09:32:24 -0500

    fdgsdfs

--- faro.py
+++ faro.py
@@ -6,14 +6,15 @@ vid = cv2.VideoCapture(0)
 while 1:
     stime = time.time()
     ret, frame = vid.read()
-
+    cv2.cvtColor(frame, cv2.COLOR_RGB2BGRA)
+    '''
     faces = fr.face_locations(frame)
 
     print(faces)
 
     if len(faces) > 0:
         cv2.rectangle(frame, (faces[0][0], faces[0][1]), (faces[0][2], faces[0][3]), (250, 10, 50), 3)
-
+    '''
     cv2.imshow('frame', frame)
 
     #print(1/(time.time()-stime))
