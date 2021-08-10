38c59b6e63535db3824bc52cd7a93eeff470d7f3
Ethan_Hadley <ekhadley@gmail.com>
2021-08-10 09:42:13 -0500

    dsfgsd

--- car_new/accessories/ardunio.c
+++ car_new/accessories/ardunio.c
@@ -5,6 +5,9 @@ Servo right;
 void setup()
 {
 Serial.begin(9600);
+pinMode(9, OUTPUT);
+pinMode(5, INPUT_PULLUP);
+pinMode(6, INPUT_PULLUP);
 left.attach(11);
 right.attach(10);
 left.write(0);
@@ -15,11 +18,22 @@ Serial.setTimeout(50);
 int ctrl = 0;
 String input;
 char next;
-
+bool brake;
+bool butt;
+bool pwr;
 
 void loop()
-
 {
+  butt = digitalRead(5);
+  brake = digitalRead(6);
+  if(butt){
+    pwr = false;
+  }
+  if(brake){
+    pwr = true;
+  }
+  digitalWrite(9, !pwr);
+  
     if(Serial.available() > 0){
         input = Serial.readString();
         ctrl = input.toInt();
@@ -28,39 +42,3 @@ void loop()
      left.write(ctrl+127);
      right.write(ctrl+127);
 }
-
-
-
-#include <Servo.h>
-Servo left;
-Servo right;
-
-void setup()
-{
-Serial.begin(9600);
-left.attach(11);
-right.attach(10);
-left.write(127);
-right.write(127);
-}
-
-int ctrl = 0;
-
-char next[4] = {'1', '4', '7'};
-
-
-void loop()
-{
-     strncat(next, '5', 1);
-     ctrl = atoi(next);
-     Serial.println(ctrl);      
-    
-     left.write(ctrl+127);
-     right.write(ctrl+127);
-}
-
-
-
-
-
-
