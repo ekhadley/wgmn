08d4954ef83399edff12dcf6292be23d7b2a4ae7
eekay <ekhadley@gmail.com>
2021-08-14 08:46:15 -0500

    dsfgdsfgsdf

--- pi/servotemplate.py
+++ pi/servotemplate.py
@@ -4,21 +4,27 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(7, GPIO.OUT)
-w = GPIO.PWM(7, 50)
-w.start(0)
+class servo():
+    def __init__(self, pin):
+        self.pin = pin
 
-def write(n, p, a):
-    duty = (a/18) + 2.5
-    GPIO.output(p, True)
-    n.ChangeDutyCycle(duty)
-    time.sleep(1)
-    n.ChangeDutyCycle(0)
+        GPIO.setup(7, GPIO.OUT)
+        self.w = GPIO.PWM(7, 50)
+        self.w.start(0)
+
+    def write(angle, self):
+        duty = (angle/18) + 2.5
+        GPIO.output(self.pin, True)
+        self.w.ChangeDutyCycle(duty)
+        time.sleep(1)
+        self.w.ChangeDutyCycle(0)
+
+x = servo(7)
 
 while 1:
-    write(w, 7, 0)
+    x.write(0)
     time.sleep(3)
-    write(w, 7, 100)
+    write(100)
     time.sleep(3)
 
 
