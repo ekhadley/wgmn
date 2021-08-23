321753efab6baf1f66102960afa78fa1abaf849e
Ethan_Hadley <ekhadley@gmail.com>
2021-08-23 10:48:11 -0500

    fdghdf

--- wonderland/collector.py
+++ wonderland/collector.py
@@ -1,24 +1,28 @@
 import requests, cv2, numpy
 
-coords = ('39.4784593', '-88.1739754')
+coords = [('39.4784593', '-88.1739754'), ('39.4784593', '-88.1739754')]
 imgsize = '2000'
 heading = '0'
 pitch = '0'
 fov = '40'
 
-url = "https://maps.googleapis.com/maps/api/streetview?size="+imgsize+"x"+imgsize+"&location="+coords[0]+","+coords[1]+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&key=AIzaSyC4CFILOjXwmKqd7ehYgUwA9SkjE0D9HlA&scale=2"
+urls = []
+for i in coords:
+    urls.append("https://maps.googleapis.com/maps/api/streetview?size="+imgsize+"x"+imgsize+"&location="+i[0]+","+i[1]+"&fov="+fov+"&heading="+heading+"&pitch="+pitch+"&key=AIzaSyC4CFILOjXwmKqd7ehYgUwA9SkjE0D9HlA")
 
-img = requests.get(url, stream=True).content
+for i in range(len(urls)):
+    img = requests.get(urls[i], stream=True).content
 
-
-with open("D:\\wonderland\\test\\1.png", "wb") as f:
-    f.write(img)
+    with open("D:\\wonderland\\test\\"+str(i)+".png", "wb") as f:
+        f.write(img)
 
 imgs = []
-for i in range(1, 3):
+for i in range(2):
     print(i)
     imgs.append(numpy.array(cv2.imread("D:\\wonderland\\test\\"+str(i)+".png")))
 
 while 1:
     for i in range(len(imgs)):
-        cv2.imshow(i, imgs[i])
+        cv2.imshow(str(i), imgs[i])
+
+    cv2.waitKey(1)
