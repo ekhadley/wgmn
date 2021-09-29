38f42e3354208e738c5f3f5bceea869ec08de074
eekay <ekhadley@gmail.com>
2021-09-29 08:29:25 -0500

    gfhjfghj

--- pi/friday.py
+++ pi/friday.py
@@ -7,26 +7,64 @@ tim = datetime.now()
 tim =  tim.strftime("%H:%M:%S")
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+
+def water():
+    plant.on()
+    time.sleep(4)
+    plant.off()
+
+kettle = servotemplate.servo(10)
+kettlebutton = servotemplate.button(8)
+plant = servotemplate.powerable(7)
+lightbutton = servotemplate.button(12)
+lights = servotemplate.powerable(11)
+beds = [servotemplate.button(35), servotemplate.button(37)]
+lightstate = 1
+
+'''
+a = servotemplate.powerable(37)
+b = servotemplate.powerable(35)
+c = servotemplate.powerable(33)
+d = servotemplate.powerable(31)
+
+steps = [a, b, c, d]
+'''
 
 live = 1
 while live: 
-    time.sleep(.05)
+    time.sleep(.06)
     tim = datetime.now().strftime("%H:%M:%S")
     sex = datetime.now().strftime("%S")
-    print(tim)
-    
-    if '18:00:00' in tim:
+
+    kettlebutton.read()
+    lbprev = lightbutton.state
+    lightbutton.read()
+    for i in beds:
+        i.read()
+        i.state = not i.state
+    for i in range(len(beds)):
+        if beds[i].state and beds[i+1].state 
+
+    print(tim, f', kettle: {kettlebutton.read()};', f'light control: {lightbutton.state};', f'bed: {bed}')
+
+    if not lbprev and lightbutton.state:
+        lightstate = not lightstate
+    if lightstate:
+        lights.on()
+    else:
+        lights.off()
+
+    if kettlebutton.state:
+        kettle.write(115)
+        kettle.write(115)
+        kettle.write(200)
+
+    if '12:30:00' in tim:
         print("WATAAAAAAAA\n(PEPEPAINS)")
-        GPIO.output(7, 1)
-        time.sleep(3)
-        GPIO.output(7, 0)
+        water()
 
-
-while not live: 
-    time.sleep(3)
-    GPIO.output(7, 1)
-    time.sleep(3)
-    GPIO.output(7, 0)
+while not live:
+    water()
+    time.sleep(10)
 
             
