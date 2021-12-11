fe7a829082f7a3b5dededd7aafccb5f7816489a0
Ethan_Hadley <ekhadley@gmail.com>
2021-12-10 21:30:40 -0600

    fghjfg

--- qlearn/dwarf.py
+++ qlearn/dwarf.py
@@ -1,8 +1,5 @@
-import cv2, numpy as np
-import tensorflow
+import cv2, tensorflow, os, keyboard as kb, time, cv2, numpy as np
 from tensorflow import keras
-
-
 
 
 class env():
@@ -10,26 +7,58 @@ class env():
         self.grid = []
         for i in range(0, 10):
             self.grid.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-        
+        self.pos = [4, 8]
+        self.grid[self.pos[1]][self.pos[0]] = 1
+        self.actionspace = ['x', 'n', 'e', 's', 'w']
+        self.action = None
+        self.done = 0
+        self.age = 0
+        self.score = 0
 
-    def step(self):
+    def choose(self):
+        self.action = 0
+        if kb.on_release_key('w'):
+            self.action = 1            
+        if kb.on_release_key('a'):
+            self.action = 4            
+        if kb.on_release_key('s'):
+            self.action = 3            
+        if kb.on_release_key('d'):
+            self.action = 2
+        print(self.action)
+        return self.actionspace[self.action]
+
+    def reset(self):
         pass
 
+    def step(self, action):
+        if action == 'N':
+            self.grid[self.pos[1]][self.pos[0]] = 0
+            self.pos[1] += 1
+        if action == 'E':
+            self.grid[self.pos[1]][self.pos[0]] = 0
+            self.pos[0] += 1
+        if action == 'S':
+            self.grid[self.pos[1]][self.pos[0]] = 0
+            self.pos[1] -= 1
+        if action == 'W':
+            self.grid[self.pos[1]][self.pos[0]] = 0
+            self.pos[0] -= 1
+
     def display(self):
-       s = ''
-       for i in range(len(self.grid)):
-           print('\n')
-           for j in range(len(self.grid[i])):
-                s += str(self.grid[i][j])
-                print(s)
+        img = np.array(self.grid)
+        print(img)
+        #cv2.imshow('img', img)
 
 
 world = env()
 
-world.display()
-
-
-
+while 1:
+    world.reset()
+    while not world.done:
+        world.choose()
+        world.step(world.choose())
+        world.display()
 
 
 
