5c0ed48606742fa3eb461aa0f38fc9d96636c40b
ekhadley <ekhadley@gmail.com>
2022-02-21 09:53:46 -0600

    Update 3dview.py

--- processing/3dview.py
+++ processing/3dview.py
@@ -1,173 +1,53 @@
-import math
 w, h = 800, 800
 def setup():
-    size(w, h)
+    global w
+    global h
     background(30)
+    size(w, h)
 
-def vshow(pos, vec, m, w):
-    strokeWeight(w)
-    stroke(m)
-    line(pos.x, pos.y, pos.x + vec.x, pos.y + vec.y)
 
-def getSpherical(p):
-    if p.x == 0 and p.y == 0:
-        return(PVector(0, 0))
-    if p.x == 0:
-        return(PVector(PI/2, atan(p.y/p.z)))
-    if p.y == 0:
-        return(PVector(0, atan(p.z/p.x)))
-    return(PVector(atan(p.x/p.y), atan((sqrt(p.x**2+p.y**2))/p.z)))
 
-def getSpherical(p):
-    return(PVector(math.atan2(p.x,p.y), math.atan2((sqrt(p.x**2+p.y**2)),p.z)))
+def getSpherical(pos):
+    theta = atan2(pos.x/pos.y)
+    phi = atan2(z/sqrt(pos.x**2+pos.y**2))
+    return PVector(theta, phi)
 
-def degreeVec(v):
-    return PVector(degrees(v.x), degrees(v.y), degrees(v.z))
-
-def getSphericalDifference(p):
-    global viewvec
-    global viewpos
-    viewPolar = getSpherical(viewvec)
-    pointPolar = getSpherical(viewpos-p)
-    sphericalDiff = pointPolar-viewPolar
-    return(sphericalDiff)
-
-class vox():
-    def __init__(self, x, y, z):
-        self.marker = color(30, 200, 30)
-        self.pos = PVector(x, y, z)
+class pointy():
+    def __init__(self, pos, marker, girth):
+        self.pos = pos
+        self.marker = marker
+        self.girth = girth
     
-    def getpos(self):
-        global w
-        global h
-        global viewvec
-        global viewpos
-        azimuth = getSphericalDifference(self.pos)
-        return PVector((azimuth.x/(1.48353*PI))*w + w/2, (azimuth.y/(1.48353*PI))*h/2)
+    def getScreenPos(self, cam):
+            diff = PVector(cam.pos.x-self.pos.x,
+                           cam.pos.y-self.pos.y,
+                           cam.pos.z-self.pos.z)
+            sphericalDiff = getSpherical(diff)
+            sphericalCamAngle = getSpherical(cam.dir.x, cam.dir.y, cam.dir.z)
+            sphericalViewAngleDiff = PVector(sphericalCamAngle.x-sphericalDiff.x,
+                                             sphericalCamAngle.y-sphericalDiff.y,)
+            return PVector(sphericalViewAngle.x*w/cam.fov, sphericalViewAngle.y*h/cam.fov)
+            
     
-    def show(self):
-        global w
-        global h
-        global viewvec
-        global viewpos
-        azimuth = getSphericalDifference(self.pos)
-        fill(self.marker)
-        noStroke()
-        ellipse((azimuth.x/(2*PI))*w + w/2, (azimuth.y/(2*PI))*h + h/2, 15, 15)
-        return PVector((azimuth.x/(1.48353*PI))*w + w/2, (azimuth.y/(1.48353*PI))*h + h/2)
-
-class beam():
-    def __init__(self, w, z):
-        self.marker = color(200, 15, 30)
-        self.end1 = w
-        self.end2 = z
-    def show(self):
-        global w
-        global h
-        global viewvec
-        global viewpos
-        end1 = w.getpos()
-        end2 = z.getpos()
-
+    def show(self, screenPos):
+        strokeWeight(self.girth)
         stroke(self.marker)
-        strokeWeight(1)
-        line(end1, endpoints[0].y, endpoints[1].x, endpoints[1].y)
-        
-        
-viewvec = PVector(-1, 1, 0)
-viewpos = PVector(1, 0, 0)
+        point(screenPos.x, screenPos.y)
 
-v = vox(0, 0, 0)
-xaxis = vox(1000, 0, 0)
-yaxis = vox(0, 1000, 0)
-zaxis = vox(0, 0, 1000)
-xb = beam(v, xaxis)
-yb = beam(v, yaxis)
-zb = beam(v, zaxis)
 
-elements = [v, xaxis, yaxis, zaxis, xb, yb, zb]
+class  cam():
+    def __init__(self, pos, dir, fov):
+        self.pos = pos
+        self.dir = dir
+        self.fov = fov
+
+kodak = cam(PVector(-10, 0, 0), PVector(1, 0, 0), 90)
+joe = pointy(0, 0, 0)
 
 def draw():
+    
+    joe.show(joe.getScreenPosition(kodak))
     background(30)
-    global viewvec
-    global viewpos
-    viewvec /= viewvec.mag()
-    
-    for i in elements:
-        i.show()
-        
-    print(viewpos)
-    
-    
-    if keyPressed and key=='w':
-        viewpos.x += .3
-    if keyPressed and key=='a':
-        viewpos.y -= .3
-    if keyPressed and key=='s':
-        viewpos.x -= .3
-    if keyPressed and key=='d':
-        viewpos.y += .3
-    if keyPressed and key=='r':
-        viewpos = PVector(1, 0, 0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
