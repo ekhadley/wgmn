58a12cb0dd99e03af8adfaebbc187c9c18ab2e5e
ekhadley <ekhadley@gmail.com>
2022-02-23 09:45:44 -0600

    Update 3dview.py

--- processing/3dview.py
+++ processing/3dview.py
@@ -1,3 +1,4 @@
+import random
 w, h = 800, 800
 def setup():
     global w
@@ -10,7 +11,7 @@ def setup():
 def getSpherical(pos):
     theta = atan2(pos.x,pos.y)
     phi = atan2(pos.z,sqrt(pos.x**2+pos.y**2))
-    print(theta, phi)
+#print(theta, phi)
     return PVector(theta, phi)
     
 
@@ -19,23 +20,24 @@ class pointy():
         self.pos = pos
         self.marker = marker
         self.girth = girth
-        self.showPos = PVector(0,0)
 
-    def getScreenPos(self, cam):
+    def show(self, cam):
             diff = PVector(cam.pos.x-self.pos.x,
                            cam.pos.y-self.pos.y,
                            cam.pos.z-self.pos.z)
+
             sphericalDiff = getSpherical(diff)
             sphericalCamAngle = getSpherical(cam.dir)
-            sphericalViewAngleDiff = PVector(sphericalCamAngle.x-sphericalDiff.x,
+            sphericalViewAngleDiff = -1*PVector(sphericalCamAngle.x-sphericalDiff.x,
                                              sphericalCamAngle.y-sphericalDiff.y,)
-            self.showPos = PVector(sphericalViewAngleDiff.x*w/cam.fov, sphericalViewAngleDiff.y*h/cam.fov)
             
+            showPos = PVector(sphericalViewAngleDiff.x*w/cam.fov, sphericalViewAngleDiff.y*h/cam.fov)
+            print(showPos)
     
-    def show(self):
-        strokeWeight(self.girth)
-        stroke(self.marker)
-        point(self.showPos.x, self.showPos.y)
+            strokeWeight(self.girth)
+            stroke(self.marker)
+            ellipse(showPos.x, showPos.y, self.girth, self.girth)
+                        
 
 
 class  cam():
@@ -44,59 +46,27 @@ class  cam():
         self.dir = dir
         self.fov = fov
 
-kodak = cam(PVector(-10, 0, 0), PVector(0, 0, 0), PI/2)
-joe = pointy(PVector(1, 1, 1), color(30, 200, 80), 5)
+    def update(self):
+        if keyPressed:
+            if key=='a':
+                self.pos.x -= .01
+            if key=='d':
+                self.pos.x += .01
+            if key=='w':
+                self.pos.z += .01
+            if key=='s':
+                self.pos.z -= .01
+    
+kodak = cam(PVector(-1, 0, 0), PVector(1, 0, 0), PI)
+
+coord = [PVector(1,0,0), PVector(0,0,1), PVector(0,1,0), PVector(0,1,1), PVector(1,0,0), PVector(1,0,1), PVector(1,1,0), PVector(1,1,1)]
+cube = []
+for i in range(len(coord)-1):
+    cube.append(pointy(coord[i], color(random.randint(0, 255),random.randint(0, 255),random.randint(0, 255)), 3))
 
 def draw():
-    
-    joe.getScreenPos(kodak)
-    joe.show()
-    
-    background(30)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    background(30)    
+    kodak.update()
+    for i in cube:
+        i.show(kodak)
 
