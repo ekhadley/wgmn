41f21f08c5c13d95ac351f6706add48f4be68357
Ethan_Hadley <ekhadley@gmail.com>
2022-04-19 10:40:44 -0500

    dfgh

--- qlearn/bond.py
+++ qlearn/bond.py
@@ -2,17 +2,22 @@ import tensorflow as tf, time, random, dwarf, bond, numpy as np
 from tensorflow import keras
 
 class agent:
-    def __init__(self, env, disc, eps, lr):
+    def __init__(self, env):
+        self.env = env
+        self.memories = []
+
+    def genModel(self, disc, eps, ler):
         self.disc = disc
         self.eps = eps
-        self.lr = lr
+        self.ler = ler
 
-        self.env = env
-
-        self.initialWeights = np.random.uniform()
         self.net = keras.models.Sequential()
-        self.net.add(keras.layers.Conv2D(32, (3,3)input_shape=(self.env.size, self.env.size, 1), activation = "relu"))
+        self.net.add(keras.layers.Conv2D(32, (3,3), input_shape=(self.env.size, self.env.size, 1), activation = "relu"))
         self.net.add(keras.layers.Flatten())
         self.net.add(keras.layers.Dense(64))
         self.net.add(keras.layers.Dense(4, activation="linear"))
-        self.net.compile(loss="mse", optimizer = keras.optimizers.Adam(lr=self.lr), metrics = ['accuracy']))
+        self.net.compile(loss="mse", optimizer = keras.optimizers.Adam(lr=self.ler), metrics = ['accuracy'])
+
+    def chooseAction(self):
+        if np.random.uniform() < self.eps:
+            return 
