5dc5419d8bc39fac6b3053ddecf8101980c0ad7b
Ethan_Hadley <ekhadley@gmail.com>
2022-05-08 18:41:34 -0500

    sdfg

--- Friggbot/frigman.py
+++ Friggbot/frigman.py
@@ -4,27 +4,14 @@ from discord.ext import tasks
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # an attribute we can access from our task
-        self.counter = 0
-
-    async def setup_hook(self) -> None:
-        # start the task to run in the background
-        self.my_background_task.start()
+        self.channel = self.get_channel(551246526924455937)  # channel ID goes here
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
 
-    @tasks.loop(seconds=5)  # task runs every 60 seconds
-    async def my_background_task(self):
-        channel = self.get_channel(551246526924455937)  # channel ID goes here
-        self.counter += 1
-        await channel.send(self.counter)
-
-    @my_background_task.before_loop
-    async def before_my_task(self):
-        await self.wait_until_ready()  # wait until the bot logs in
+    async def on_message(self, message):
+        if "ozzy" in message.content:
+           await self.channel.send("yes, this is ozzy speaking")
 
 client = MyClient()
 client.run('MzUyMjI2MDQ1MjI4ODc1Nzg2.YngUnA.hORVUTyrazkOOm-6iF-A6XIHJCU')
