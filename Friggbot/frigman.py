7204210a206a0fa0ca0fd42dde761f8855d17e46
ekhadley <ekhadley@gmail.com>
2022-05-08 19:05:23 -0500

    j

--- Friggbot/frigman.py
+++ Friggbot/frigman.py
@@ -4,14 +4,14 @@ from discord.ext import tasks
 class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.channel = self.get_channel(551246526924455937)  # channel ID goes here
+        self.channel = self.get_channel(551246526924455937)
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
 
     async def on_message(self, message):
-        if "ozzy" in message.content:
+        if "ozzy" == message.content:
            await self.channel.send("yes, this is ozzy speaking")
 
 client = MyClient()
-client.run('MzUyMjI2MDQ1MjI4ODc1Nzg2.YngUnA.hORVUTyrazkOOm-6iF-A6XIHJCU')
+client.run('MzQ1MTM1NTA1MDQzNzUwOTEy.YnhX3w.UGCIEk-X_c-soXgTvv56u6GeTo8')
