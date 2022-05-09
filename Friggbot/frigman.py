a8a42351ae97601b954ced1bf8da23fdf32431ee
ekhadley <ekhadley@gmail.com>
2022-05-08 21:26:57 -0500

    f

--- Friggbot/frigman.py
+++ Friggbot/frigman.py
@@ -5,6 +5,7 @@ class MyClient(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.channel = self.get_channel(551246526924455937)
+        #self.channel = self.get_channel(785014933758410793)
 
     async def on_ready(self):
         print(f'Logged in as {self.user} (ID: {self.user.id})')
@@ -14,4 +15,4 @@ class MyClient(discord.Client):
            await self.channel.send("yes, this is ozzy speaking")
 
 client = MyClient()
-client.run('MzUyMjI2MDQ1MjI4ODc1Nzg2.YnhbAA.G_yITzIf18Eu6p7Y29grnKqBlHs')
+client.run('MzUyMjI2MDQ1MjI4ODc1Nzg2.Ynh7nA.iZSWXUI2SL3bdH4TY1mliDcsgKY')
