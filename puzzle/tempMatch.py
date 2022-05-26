bdc1ef703737da105d00c05b2c7341923f4c403b
Ethan_Hadley <ekhadley@gmail.com>
2022-05-26 13:18:50 -0500

    d

--- puzzle/tempMatch.py
+++ puzzle/tempMatch.py
@@ -1,31 +1,32 @@
 from PIL import Image
 import cv2, numpy as np
-from funcs import *
+from functions import *
 
-pc = np.array(cv2.imread("C:\\Users\\ek\\Desktop\\sdfghj\\puzzle\\testimgs\\candy\\sub8.jpg", cv2.IMREAD_GRAYSCALE))
-target = np.array(cv2.imread("C:\\Users\\ek\\Desktop\\sdfghj\\puzzle\\testimgs\\candy\\reference.jpg", cv2.IMREAD_GRAYSCALE))
+pc = np.array(cv2.imread("C:\\Users\\ek\\Desktop\\sdfghj\\puzzle\\testimgs\\candy\\pc4cr.jpg", cv2.IMREAD_GRAYSCALE))
+ref = np.array(cv2.imread("C:\\Users\\ek\\Desktop\\sdfghj\\puzzle\\testimgs\\candy\\reference.jpg", cv2.IMREAD_GRAYSCALE))
 
-#pc = imscale(pc, 3/8)
-
+#pc = imscale(pc, 1/4)
 
 pcs = [pc]
-for i in range(0, 3):
-    pc = np.rot90(pc)
-    pcs.append(pc)
+dims = [np.shape(pc)]
+for i in range(1, 21):
+    x = imscale(pc, i/20)
+    for j in range(0, 4):
+        x = np.rot90(x)
+        pcs.append(x)
+        dims.append(np.shape(x))
 
-matches = multiMatch(target, pcs)
-target = rectangles(cv2.cvtColor(target, cv2.COLOR_GRAY2BGR), matches[:,0], (len(pc[0]), len(pc)), weight=10)
-print(matches)
-#pos, sim, map = match(target, pc, returnMap=True)
-#target = cv2.rectangle(cv2.cvtColor(target, cv2.COLOR_GRAY2BGR), pos, (pos[0] + len(pc[0]), pos[1] + len(pc)), (100, 0, 255), 10)
+m = multiMatch(ref, pcs, bestOnly=True)
+print(m)
 
+ref = rectangles(cv2.cvtColor(ref, cv2.COLOR_GRAY2BGR), [m[0]], np.shape(pc))
 
 while 1:
     for i, p in enumerate(pcs):
         cv2.imshow(f"pc{i}", imscale(p, 1.2))
 
 #    cv2.imshow("map", imscale(map, .2))
-    cv2.imshow("target", imscale(target, .35))
+    cv2.imshow("target", imscale(ref, .35))
 #    cv2.imshow("matched", imscale(res, .1))
 
     if cv2.waitKey(1) & 0xFF == ord('q'): 
