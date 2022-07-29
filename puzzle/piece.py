f2d0d3fecc93ebcdd4efd4546c069c06ff7f9da7
Ethan_Hadley <ekhadley@gmail.com>
2022-07-29 07:31:58 -0500

    sdfg

--- puzzle/piece.py
+++ puzzle/piece.py
@@ -21,7 +21,8 @@ class pc:
         return contours[0]
 
     def findCorners(self):
-        cornerMap = cv2.cornerHarris(self.im, 10, 5, .001)
+        #cornerMap = cv2.cornerHarris(self.im, 10, 5, .001)
+        cornerMap = cv2.cornerHarris(self.im, 25, 5, .001)
         #ret, bina = cv2.threshold(cornerMap, .25*np.max(cornerMap), 255, cv2.THRESH_BINARY)
         y, x = np.where(cornerMap>.5*np.max(cornerMap))
         self.cm = cornerMap
@@ -66,7 +67,8 @@ class pc:
         corners = cv2.convexHull(np.array(self.corners, np.float32))
         #corners = np.array(self.corners, np.float32)
         #rect = cv2.convexHull(np.array([[x, y], [x, y+h], [x+w, y+h], [x+w, h]], np.float32))
-        rect = np.array([[80, 80], [320, 80], [320, 320], [80, 320]], np.float32)
+        sqrSize = 2000
+        rect = np.array([[80, 80], [sqrSize-80, 80], [sqrSize-20, sqrSize-80], [80, sqrSize-80]], np.float32)
         mat = cv2.getPerspectiveTransform(corners, rect)
         shifted = [[], [], [], []]
         for i, side in enumerate(self.sides):
@@ -74,10 +76,10 @@ class pc:
                 shifted[i].append([(mat[0][0]*p[0] + mat[0][1]*p[1] + mat[0][2])/(mat[2][0]*p[0] + mat[2][1]*p[1] + mat[2][2]),
                                    (mat[1][0]*p[0] + mat[1][1]*p[1] + mat[1][2])/(mat[2][0]*p[0] + mat[2][1]*p[1] + mat[2][2])])
         s = np.shape(self.base)
-        self.warped = cv2.warpPerspective(self.base, mat, (400, 400))
+        self.warped = cv2.warpPerspective(self.base, mat, (sqrSize, sqrSize))
         return shifted
     
-    def preprocess(self, im, undistort, lower=15_000, upper=100_000):
+    def preprocess(self, im, undistort, lower=1_000_000, upper=2_000_000):
         if undistort != None:
             mtx,dst,newmtx = undistort
             im = cv2.undistort(im, mtx, dst, None, newmtx)
@@ -116,22 +118,11 @@ class pc:
             mask[i] = 1 if (sum(c)/len(c))<5 else 0
         return mask
     
-    def show(self, base=False, scale=1, edges = True, corners = True, center = True, locks=False, thickness=2):
+    def show(self, base=False, scale=1, edges = True, corners = True, center = True, locks=False, radius=8,thickness=2):
         if base:
             mod = np.copy(self.base)
         else:
             mod = cv2.cvtColor(self.im, cv2.COLOR_GRAY2BGR)
-
-        #(x,y),radius = minEnclosingCircle(self.edge)
-        #center = (int(x),int(y))
-        #radius = int(radius)
-        #cv2.circle(mod,center,radius,(0,255,0),2)
-        
-        #box = cv2.boxPoints(rect)
-        #box = np.int0(box)
-        #hull = cv2.convexHull(self.corners)
-        #cv2.drawContours(mod,[box],0,(0,0,255),2)
-        #mod = cv2.polylines(mod, np.int32([hull]), True, (0, 255, 0), 2)
 
         if (edges and len(self.edge) == 0) or corners and len(self.corners) == 0 or (center and self.centroid == None) or (locks and len(self.locks)==0):
             print("(requested elements have not been detected)")
@@ -145,14 +136,13 @@ class pc:
                 #mod = cv2.drawContours(mod, [self.edge], -1, (250, 150, 0), 3)
                 mod = cv2.polylines(mod, np.int32([self.edge]), False, (250-70*i, 150-50*i, 80*i), thickness)
         if corners:
-            c = (sum(self.corners[:,0]/len(self.corners[:,0])), sum(self.corners[:,1]/len(self.corners[:,1])))
             for i, e in enumerate(self.corners):
-                mod = circles(mod, [e], radius=8, width=2, color=(250-70*i, 150-50*i, 80*i))
+                mod = circles(mod, [e], radius=radius, width=thickness, color=(250-70*i, 150-50*i, 80*i))
         if center:
-            mod = cv2.circle(mod, (round(self.centroid[0]), round(self.centroid[1])), 10, (130, 255, 50), 2)
+            mod = cv2.circle(mod, (round(self.centroid[0]), round(self.centroid[1])), radius, (130, 255, 50), thickness)
         if locks:
             for e in self.locks:
-                mod = circles(mod, e, radius=5, width=1, color=(190, 255, 45))
+                mod = circles(mod, e, radius=radius, width=thickness, color=(190, 255, 45))
         return imscale(mod, scale)
 
 class puzzle:
@@ -164,8 +154,7 @@ class puzzle:
         self.edgepcs = [e for e in self.pcs if sum(e.straightSides) == 1]
         self.cornerpcs = [e for e in self.pcs if sum(e.straightSides) == 2]
         self.solved = False
-        c=0
-        self.place(self.pcs[0], (0,0), c)
+        self.place(self.pcs[0], (0,0), 0)
 
     def getBorders(self, pos):
         x, y = pos[0], pos[1]
@@ -173,7 +162,7 @@ class puzzle:
         borders = [None, None, None, None]
         for pc in self.placed:
             if pc.pos in c:
-                s = indexOf(c, pc.pos)
+                s = c.index(pc.pos)
                 borders[(s+2)%4] = (pc, (s+pc.rotation)%4)
         return borders 
 
@@ -223,20 +212,20 @@ class puzzle:
                         borders[4-m].append((i, j))
         return [e for e in borders if len(e)>0]
 
-    def evalMatch(self, pieces, sidenums, show=False):
+    def evalMatch(self, pieces, sidenums, show=False, thickness=1):
         first, second = sidenums
         pc1, pc2 = pieces
         s1, s2 = pc1.correctedSides[first], pc2.correctedSides[second]
-        s1o, s2o = pc1.sides[first], pc2.sides[second]
         origin = [-200, -200]
-#        s1, s2 = shiftPts(s1, s1[0]), shiftPts(np.flipud(s2), s2[-1])
+        #s1, s2 = shiftPts(s1, s1[0]), shiftPts(np.flipud(s2), s2[-1])
         s1, s2 = shiftPts(s1, s1[0]), shiftPts(s2, s2[0])
         offset = math.atan2(s1[-1][1], s1[-1][0]) - math.atan2(s2[-1][1], s2[-1][0])
         s2 = [rotateby(e, offset) for e in s2]
 
+        s1o, s2o = pc1.sides[first], pc2.sides[second]
         d = dist(s1o[0], s1o[-1]) - dist(s2o[0], s2o[-1])
-        smask1, smask2 = pc1.straightSides, pc2.straightSides
 
+        smask1, smask2 = pc1.straightSides, pc2.straightSides
         if (1 in smask1) and (1 in smask2):
             check = not ((smask1[(first-1)%4] and smask2[(second+1)%4]) or (smask1[(first+1)%4] and smask2[(second-1)%4]))
         elif (1 in smask1) and (not 1 in smask2):
@@ -251,13 +240,13 @@ class puzzle:
         if d > 15 or smask1[first] or smask2[second] or check:
             fit = 1000
         else:
-            fit = similaritymeasures.frechet_dist(s1, s2)
-            #fit = listSim(s1, s2)
+            #fit = similaritymeasures.frechet_dist(s1, s2)
+            fit = listSim(s1, s2)
         if show:
-            shape = np.shape(pc1.im)
-            im = np.zeros((shape[0], shape[1], 3), np.uint8)
+            x, y = np.shape(pc1.im)
+            im = np.zeros((x, y, 3), np.uint8)
             s1, s2 = shiftPts(s1, origin), shiftPts(s2, origin)
-            im = cv2.polylines(im, np.int32([s1]), False, (250, 0, 50), 1)
-            im = cv2.polylines(im, np.int32([s2]), False, (50, 0, 250), 1)
+            im = cv2.polylines(im, np.int32([s1]), False, (250, 0, 50), thickness)
+            im = cv2.polylines(im, np.int32([s2]), False, (50, 0, 250), thickness)
             return fit, im
         return fit
